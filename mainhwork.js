29c6fbe33a058a5fc8c45d034df0e1cd34100af3
Tigerminde <tevanyanjan85@mail.ru>
2019-11-04 16:40:40 +0300

    jswork2

--- mainhwork.js
+++ mainhwork.js
@@ -1,6 +1,17 @@
+//задача 1
+
 let algebra = 5;
 let geography = 4;
 let physics = 2;
 let rating;
 rating = (algebra + geography + physics) / 3;
 console.log("Средняя оценка " + rating + " балла");
+
+
+//задача 2
+
+let name = 'Тигран';
+let message;
+
+message = ("Привет, мир! Меня зовут " + name );
+console.log(message);
